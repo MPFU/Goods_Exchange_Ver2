aee394138d6ce3652233f44fc8fe5677a9193948
FelixNguye <utphucnguyen@gmail.com>
2024-06-02 22:43:41 +0700

    CRUD
    Signed-off-by: FelixNguye <utphucnguyen@gmail.com>

--- goods-server.Contracts/OrderDetailDTO.cs
+++ goods-server.Contracts/OrderDetailDTO.cs
@@ -11,8 +11,8 @@
 
     public class CreateOrderDetailDTO
     {
-        public Guid OrderId { get; set; }
-        public Guid ProductId { get; set; }
+        public Guid? OrderId { get; set; }
+        public Guid? ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
